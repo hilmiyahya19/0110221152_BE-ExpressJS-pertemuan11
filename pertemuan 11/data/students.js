1d2bb65e171736d453815a68bfe49c0ce640e5f2
hilmiyahya19 <91936796+hilmiyahya19@users.noreply.github.com>
2022-12-18 11:00:26 +0700

    Add files via upload

---
+++ pertemuan 11/data/students.js
@@ -0,0 +1,7 @@
+// TODO 1: Buat data students
+// code here
+const students = ["Hilmi Yahya", "Husnul Huda", "Hafidz Ammar"];
+
+// TODO 2: export data students
+// code here
+module.exports = students;
